5e8ce6acd787f087577f925c5c5b91e8515a0912
Corey Wright <115733270+CoreyWright476@users.noreply.github.com>
2022-11-08 23:08:10 +0000

    basic html files linked to individual css docs. no content as of yet

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,16 @@
-from flask import Flask
+from flask import render_template
 
 from application import app
 
+@app.route('/')
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Please sign in')
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/recipies')
+def recipies():
+    return render_template('recipies.html')
